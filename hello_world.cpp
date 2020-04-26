91c0b90e29d5945db137da1364f7c9a5476e1c59
callmemiya <mari.zabrodina.01@mail.ru>
2020-04-26 18:02:53 +0300

    correct program

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,10 @@
-#include <iostream>
+include <iostream>
 int main(){
 std::cout <<"username:";
 char name[15];
 std::cin.getline(name,15);
-std::cout <<"Hello world from";
+std::cout <<"Hello world from ";
 for (int i=0;i<strlen(name);i++) std::cout <<name[i];
+std::cout <<endl;
 return 0;
 }
