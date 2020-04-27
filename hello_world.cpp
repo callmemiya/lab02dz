e5ee11ee407b19f4df26a74b6dbaf57e8984332a
callmemiya <mari.zabrodina.01@mail.ru>
2020-04-27 14:12:13 +0300

    without u.n.std

--- hello_world.cpp
+++ hello_world.cpp
@@ -5,6 +5,6 @@ char name[15];
 std::cin.getline(name,15);
 std::cout <<"Hello world from ";
 for (int i=0;i<strlen(name);i++) std::cout <<name[i];
-std::cout <<endl;
+std::cout <<"\n";
 return 0;
 }
