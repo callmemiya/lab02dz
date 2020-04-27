3590175fedd49637408ef684ed6efec177c61f7d
callmemiya <mari.zabrodina.01@mail.ru>
2020-04-27 14:23:10 +0300

    add comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 int main(){
-std::cout <<"username:";
-char name[15];
-std::cin.getline(name,15);
+std::cout <<"username:";//запрашиваем имя пользователя
+char name[15];//создаем массив под имя
+std::cin.getline(name,15)//вводим имя пользователя и записываем в массив name
 std::cout <<"Hello world from ";
-for (int i=0;i<strlen(name);i++) std::cout <<name[i];
+for (int i=0;i<strlen(name);i++) std::cout <<name[i];//выводим имя
 std::cout <<"\n";
 return 0;
 }
